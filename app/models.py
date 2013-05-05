da10125bde6d9228c35e0c67513fa4a84dbd07fa
Matt Rosenthal <matt@casualplans.com>
2013-05-05 02:03:33 -0700

    microblog tutorial part 8 complete

--- app/models.py
+++ app/models.py
@@ -4,6 +4,12 @@ from hashlib import md5
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)	
+
+
 class User(db.Model):
 	"""model to represent users in our database"""
 	id = db.Column(db.Integer, primary_key = True)
@@ -13,7 +19,13 @@ class User(db.Model):
 	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 	about_me = db.Column(db.String(140))
 	last_seen = db.Column(db.DateTime)
-	
+	followed = db.relationship('User', 
+	secondary = followers, 
+	primaryjoin = (followers.c.follower_id == id), 
+	secondaryjoin = (followers.c.followed_id == id), 
+	backref = db.backref('followers', lazy = 'dynamic'), 
+	lazy = 'dynamic')
+		
 	@staticmethod
 	def make_unique_nickname(nickname):
 		if User.query.filter_by(nickname = nickname).first() == None:
@@ -42,6 +54,22 @@ class User(db.Model):
 	def get_id(self):
 		return unicode(self.id)
 
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+			return self
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+			return self
+
+	def is_following(self, user):
+		return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+	
+	def followed_posts(self):
+		return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())	
+
 	def __repr__(self):
 		return '<User %r>' % (self.nickname)
 
@@ -53,4 +81,5 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return '<Post %r>' % (self.body)
+	
 		
--- app/views.py
+++ app/views.py
@@ -66,15 +66,17 @@ def after_login(resp):
 	if resp.email is None or resp.email == "":
 		flash('Invalid login. Please try again.')
 		redirect(url_for('login'))
-		return "Could not login user"
-	print "response email = " + str(resp.email)
 	user = User.query.filter_by(email = resp.email).first()
 	if user is None:
 		nickname = resp.nickname
 		if nickname is None or nickname == "":
 			nickname = resp.email.split('@')[0]
+		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
 		db.session.add(user)
+		db.session.commit()
+		# make the user follow him/herself
+		db.session.add(user.follow(user))
 		db.session.commit()
 	remember_me = False
 	if 'remember_me' in session:
@@ -127,6 +129,43 @@ def edit():
 		form.about_me.data = g.user.about_me
 	return render_template('edit.html',
 						form = form)	
+
+@app.route('/follow/<nickname>')
+def follow(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t follow yourself!')
+		return redirect(url_for('user', nickname = nickname))
+	u = g.user.follow(user)
+	if u is None:
+		flash('Cannot follow ' + nickname + '.')
+		return redirect(url_for('user', nickname = nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You are now following ' + nickname + '!')
+	return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+def unfollow(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	if user == g.user:
+		flash('You can\'t unfollow yourself!')
+		return redirect(url_for('user', nickname = nickname))
+	u = g.user.unfollow(user)
+	if u is None:
+		flash('Cannot unfollow ' + nickname + '.')
+		return redirect(url_for('user', nickname = nickname))
+	db.session.add(u)
+	db.session.commit()
+	flash('You have stopped following ' + nickname + '.')
+	return redirect(url_for('user', nickname = nickname))
+
 	
 	
 @app.errorhandler(404)
