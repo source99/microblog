db08d85bf6a0f2acb3049cd1753165931ae9bf19
Matt Rosenthal <matt@casualplans.com>
2013-05-05 01:16:33 -0700

    microblog tutorial section 7 complete

--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm
+from forms import LoginForm, EditForm
 from models import User, ROLE_USER, ROLE_ADMIN
+from datetime import datetime
 
+@app.route('/favicon.ico')
+def favicon():
+	return " "
 
 @app.route('/')
 @app.route('/index')
@@ -32,6 +36,10 @@ def load_user(id):
 @app.before_request
 def before_request():
 	g.user = current_user
+	if g.user.is_authenticated():
+		g.user.last_seen = datetime.utcnow()
+		db.session.add(g.user)
+		db.session.commit()
 
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
@@ -84,3 +92,48 @@ def logout():
 def fake_id():
 	print "matt.rosenthal@gmail.com"
 	return "matt.rosenthal@gmail.com"
+
+
+@app.route('/user/<nickname>')
+@login_required
+def user(nickname):
+	user = User.query.filter_by(nickname = nickname).first()
+	if user == None:
+		flash('User ' + nickname + ' not found.')
+		return redirect(url_for('index'))
+	posts = [
+			{'author' : user, 'body' : 'Test post #1'},
+			{'author' : user, 'body' : 'Test post #2'},
+			]
+	return render_template('user.html',
+						user = user,
+						posts = posts)
+	
+	
+	
+@app.route('/edit', methods = ['GET', 'POST'])
+@login_required
+def edit():
+	form = EditForm(g.user.nickname)
+	if form.validate_on_submit():
+		g.user.nickname = form.nickname.data
+		g.user.about_me = form.about_me.data
+		db.session.add(g.user)
+		db.session.commit()
+		flash('Your changes have been saved.')
+		return redirect(url_for('edit'))
+	else:
+		form.nickname.data = g.user.nickname
+		form.about_me.data = g.user.about_me
+	return render_template('edit.html',
+						form = form)	
+	
+	
+@app.errorhandler(404)
+def internal_error(error):
+	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+	db.session.rollback()
+	return render_template('500.html'), 500
--- config.py
+++ config.py
@@ -15,3 +15,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 2345
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['matt@localhost.com']
